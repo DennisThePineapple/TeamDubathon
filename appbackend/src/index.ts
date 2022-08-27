6da40d750defba11f2adbaa944ce006e33650de9
Dennis <andennis07@gmail.com>
2022-08-27 14:01:44 +1000

    Updated schema

--- appbackend/src/index.ts
+++ appbackend/src/index.ts
@@ -1,13 +1,9 @@
-import './pre-start'; // Must be the first import
+import app from '@server';
 import logger from 'jet-logger';
-import server from './server';
 
 
-// Constants
-const serverStartMsg = 'Express server started on port: ',
-        port = (process.env.PORT || 3000);
-
-// Start server
-server.listen(port, () => {
-    logger.info(serverStartMsg + port);
+// Start the server
+const port = Number(process.env.PORT || 3000);
+app.listen(port, () => {
+    logger.info('Express server started on port: ' + port);
 });
--- appbackend/src/server.ts
+++ appbackend/src/server.ts
@@ -1,76 +1,45 @@
-import cookieParser from 'cookie-parser';
 import morgan from 'morgan';
-import path from 'path';
 import helmet from 'helmet';
 
 import express, { NextFunction, Request, Response } from 'express';
 import StatusCodes from 'http-status-codes';
 import 'express-async-errors';
 
-import apiRouter from './routes/api';
+import BaseRouter from './routes';
 import logger from 'jet-logger';
-import { CustomError } from '@shared/errors';
 
-
-// Constants
 const app = express();
+const { BAD_REQUEST } = StatusCodes;
 
 
-/***********************************************************************************
- *                                  Middlewares
- **********************************************************************************/
 
-// Common middlewares
+/************************************************************************************
+ *                              Set basic express settings
+ ***********************************************************************************/
+
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-app.use(cookieParser());
-
 // Show routes called in console during development
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
 }
 
-// Security (helmet recommended in express docs)
+// Security
 if (process.env.NODE_ENV === 'production') {
     app.use(helmet());
 }
 
+// Add APIs
+app.use(BaseRouter);
 
-/***********************************************************************************
- *                         API routes and error handling
- **********************************************************************************/
-
-// Add api router
-app.use('/api', apiRouter);
-
-// Error handling
-app.use((err: Error | CustomError, _: Request, res: Response, __: NextFunction) => {
+// Print API errors
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
     logger.err(err, true);
-    const status = (err instanceof CustomError ? err.HttpStatus : StatusCodes.BAD_REQUEST);
-    return res.status(status).json({
+    return res.status(BAD_REQUEST).json({
         error: err.message,
     });
 });
 
-
-/***********************************************************************************
- *                                  Front-end content
- **********************************************************************************/
-
-// Set views dir
-const viewsDir = path.join(__dirname, 'views');
-app.set('views', viewsDir);
-
-// Set static dir
-const staticDir = path.join(__dirname, 'public');
-app.use(express.static(staticDir));
-
-// Serve index.html file
-app.get('*', (_: Request, res: Response) => {
-    res.sendFile('index.html', {root: viewsDir});
-});
-
-
-
-// Export here and start in a diff file (for testing).
+// Export express instance
 export default app;
