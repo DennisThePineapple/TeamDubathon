c642a2cadcab4105d796e032138d376624dfcbbc
BozenX <bozen0@hotmail.com>
2022-08-27 21:04:54 +1000

    on click change spinning face

--- webapp/src/Components/LoadingSpinner.tsx
+++ webapp/src/Components/LoadingSpinner.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import '../App.css';
 import faces from "../Const/Faces";
+import Stop from "../Types/Stop";
 
 type loadingSpinnerProps = {
     isAppLogo : boolean
@@ -8,10 +9,20 @@ type loadingSpinnerProps = {
 
 export default function LoadingSpinner(props : loadingSpinnerProps) {
 
-    const name = faces[Math.floor(Math.random() * faces.length)];
-    const logo = require('../Images/' + name + '.png');
+    const [name, setName] = useState<string>("dennis");
+    const [logo, setLogo] = useState();
+
+    const changeLogo = () => {
+        setName(faces[Math.floor(Math.random() * faces.length)]);
+        setLogo(require('../Images/' + name + '.png'));
+    }
+
+    useEffect(() => {
+        changeLogo()
+    }, [])
+
     return (
-        <div className = "spinner-container">
+        <div className = "spinner-container" onClick={changeLogo}>
             <div className = "loading-spinner">
                 <img src={logo} className={props.isAppLogo ?  "App-logo" : "Spinner-logo"} alt="logo" />
             </div>
