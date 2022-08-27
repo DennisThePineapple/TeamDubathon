f44036f40378bf9850676c9f633e57b8408a9128
Mark Oh <dhfis48@outlook.com>
2022-08-27 22:48:41 +1000

    converting epoch time

--- scraper/config.py
+++ scraper/config.py
@@ -1,4 +1,5 @@
-DB_STRING = "postgresql://postgres:password@dubathon.cjilpjpquw6h.ap-southeast-2.rds.amazonaws.com:5432/gtfs"
+DB_STRING = "postgresql://postgres:password@dubathon.cjilpjpquw6h.ap-southeast-2.rds.amazonaws.com:5432/dubathon"
+
 TRIP_UPDATES_URL = "https://gtfsrt.api.translink.com.au/api/realtime/SEQ/TripUpdates"
 VEHICLE_POSITIONS_URL = "https://gtfsrt.api.translink.com.au/api/realtime/SEQ/VehiclePositions"
 ALERTS_URL = "https://gtfsrt.api.translink.com.au/api/realtime/SEQ/alerts"
