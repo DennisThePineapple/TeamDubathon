9f6fd83a0677795ed7f1f052cf4fb13f1866c54c
Mark Oh <dhfis48@outlook.com>
2022-08-27 16:38:10 +1000

    .

---
+++ scraper/config.py
@@ -0,0 +1,4 @@
+DB_STRING = "postgresql://postgres:password@dubathon.cjilpjpquw6h.ap-southeast-2.rds.amazonaws.com:5432/gtfs"
+TRIP_UPDATES_URL = "https://gtfsrt.api.translink.com.au/api/realtime/SEQ/TripUpdates"
+VEHICLE_POSITIONS_URL = "https://gtfsrt.api.translink.com.au/api/realtime/SEQ/VehiclePositions"
+ALERTS_URL = "https://gtfsrt.api.translink.com.au/api/realtime/SEQ/alerts"
