a0d5c89686ecfb03e032d7ae23a00f81c6ddd897
Dennis <andennis07@gmail.com>
2022-08-28 13:29:55 +1000

    Sizing and extra chart

--- webapp/src/Components/Charts/ChartContainer.tsx
+++ webapp/src/Components/Charts/ChartContainer.tsx
@@ -3,7 +3,6 @@ import Stop from "../../Types/Stop";
 import Route from "../../Types/Route";
 import VehiclePosition from "../../Types/VehiclePosition";
 import API from "../../Api/API";
-import StopTime from "../../Types/StopTime";
 import {WeekChart} from "./WeekChart";
 import {Typography} from "@material-ui/core";
 
@@ -15,20 +14,18 @@ type chartContainerProps = {
 
 export default function ChartContainer(props: chartContainerProps) {
     const [vehiclePositions, setVehiclePositions] = useState<VehiclePosition[]>()
-    const [stopTimes, setStopTimes] = useState<StopTime[]>()
+    const [allVehiclePositions, setAllVehiclePositions] = useState<VehiclePosition[]>()
 
-
-    const data = stopTimes ? stopTimes.map(stopTime => stopTime.arrival_time) : []
 
     useEffect(() => {
         API.getVehiclePositionsForRouteStop(props.route.route_id, props.selectedStop.stop_id).then(
             res => {
                 setVehiclePositions(res)
-                API.getStopTimesForRouteStop(res ? res.map(vehiclePosition => vehiclePosition.trip_id) : [""], props.selectedStop.stop_id).then(
-                    res2 => setStopTimes(res2)
-                )
             }
         )
+        API.getVehiclePositionsForRoute(props.route.route_id).then(res => {
+                setAllVehiclePositions(res)
+        })
     }, [])
 
     const getAvg = (arr: number[]): number => {
@@ -42,13 +39,22 @@ export default function ChartContainer(props: chartContainerProps) {
     }
 
     const renderCharts = () => {
-        if (vehiclePositions) {
+        if (vehiclePositions && allVehiclePositions) {
             return (
-                <div>
-                    <Typography>
-                        Average Delay For this Stop: {getAvg(vehiclePositions.map(p => p.delay))}
-                    </Typography>
-                    <WeekChart vehiclePosition={vehiclePositions}/>
+                <div className = "chart-container">
+                    <div className = "chart">
+                        <Typography>
+                            Average Delay For this Stop: {getAvg(vehiclePositions.map(p => p.delay))}
+                        </Typography>
+                        <WeekChart vehiclePosition={vehiclePositions}/>
+                    </div>
+
+                    <div className = "chart">
+                        <Typography>
+                            Average Delay For this Route: {getAvg(allVehiclePositions.map(p => p.delay))}
+                        </Typography>
+                        <WeekChart vehiclePosition={allVehiclePositions}/>
+                    </div>
                 </div>
             )
         }
@@ -56,7 +62,7 @@ export default function ChartContainer(props: chartContainerProps) {
 
     return (
 
-        <div className="chart-container">
+        <div>
             {renderCharts()}
         </div>
     )
